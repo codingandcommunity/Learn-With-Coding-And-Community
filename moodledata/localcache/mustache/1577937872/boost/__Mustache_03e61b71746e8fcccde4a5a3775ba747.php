057d56c2ce3c8ed97ac3718edc2f7241b3e21622
JessHua159 <jesseweihuang@gmail.com>
2020-01-01 22:29:40 -0800

    Added Moodledata Folder

---
+++ moodledata/localcache/mustache/1577937872/boost/__Mustache_03e61b71746e8fcccde4a5a3775ba747.php
@@ -0,0 +1,14 @@
+<?php
+
+class __Mustache_03e61b71746e8fcccde4a5a3775ba747 extends Mustache_Template
+{
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $buffer = '';
+
+        $buffer .= $indent . '<div class="toast-wrapper mx-auto py-3 fixed-top" role="status" aria-live="polite"></div>
+';
+
+        return $buffer;
+    }
+}
